adfd4683bd4400ce432a61ed8fdef4062cd7d8b6
solzycoded <solomonfidelis012@gmail.com>
2024-02-23 21:58:26 +0100

    included a visual confirmation when a category is selected

--- js/sell-an-item-category.js
+++ js/sell-an-item-category.js
@@ -1,6 +1,11 @@
-// script.js 
-document.addEventListener("DOMContentLoaded", function () { 
-    const setOnClickEvent = () => {
-        
-    }
-});
+const onCategorySelected = (target) => {
+    // remove "selected-category" class from all elements that match "sell-an-item-category-item"
+    performActionOnClassNames("sell-an-item-category-item", removeSelectedCategoryFromTarget);
+
+    // add "selected-category" to the target
+    target.classList.add("selected-category");
+}
+
+const removeSelectedCategoryFromTarget = (target) => {
+    target.classList.remove("selected-category");
+}
