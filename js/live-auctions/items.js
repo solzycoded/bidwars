2af7937eb2d7bbb74a34c86da190c4f700c740a3
solzycoded <solomonfidelis012@gmail.com>
2024-03-12 23:12:21 +0100

    displayed a list of items in the live auction section on the home page

--- js/live-auctions/items.js
+++ js/live-auctions/items.js
@@ -1,19 +1,43 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const loadItems = () => {
-        new FetchRequest("GET", "api/items/live").send(listOfItems, listOfItems);
-    }
+const { createApp, ref } = Vue
 
-    const listOfItems = (items) => {
-        const liveAuctionItemsSection = getElementById("live-auctions-items-section");
-        const classList               = liveAuctionItemsSection.classList;
-
-        if(items.length > 0){
-            classList.remove("d-none");
+createApp({
+    data() {
+        return {
+            liveAuctionItems: [],
+            hasLiveItems: false
         }
-        else{
-            classList.add("d-none");
+    },
+    methods: {
+        loadItems() {
+            new FetchRequest("GET", "api/items/live").send(this.listOfItems, this.listOfItems);
+        },
+        listOfItems(items) {
+            this.liveAuctionItems = items;
+            this.hasLiveItems     = items.length > 0;
         }
+    },
+    mounted: function(){
+        this.loadItems();
     }
+}).mount('#main-section');
 
-    loadItems();
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//     const loadItems = () => {
+//         new FetchRequest("GET", "api/items/live").send(listOfItems, listOfItems);
+//     }
+
+//     const listOfItems = (items) => {
+//         // console.log(items);
+//         // const liveAuctionItemsSection = getElementById("live-auctions-items-section");
+//         // const classList               = liveAuctionItemsSection.classList;
+
+//         // if(items.length > 0){
+//         //     classList.remove("d-none");
+//         // }
+//         // else{
+//         //     classList.add("d-none");
+//         // }
+//     }
+
+//     loadItems();
+// });
