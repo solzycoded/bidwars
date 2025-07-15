76c8f50d3d2b42d5894fed165bd31914a5a85a60
solzycoded <solomonfidelis012@gmail.com>
2025-07-15 20:45:19 +0100

    include prop-types for props validation in the components, LinkItem and NavItem

--- src/components/Nav/LinkItem.js
+++ src/components/Nav/LinkItem.js
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 const LinkItem = ({ href, content }) => {
     return (
         <li className="nav-item">
@@ -6,6 +8,11 @@ const LinkItem = ({ href, content }) => {
                 href={`#${href}`}>{content}</a>
         </li>
     )
+}
+
+LinkItem.propTypes = {
+    href: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired
 }
 
 export default LinkItem;
--- src/components/Nav/NavItem.js
+++ src/components/Nav/NavItem.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 const NavItem = ({ to, content, className }) => {
     return (
@@ -9,6 +10,12 @@ const NavItem = ({ to, content, className }) => {
                 to={`/${to}`}>{content}</Link>
         </li>
     )
+}
+
+NavItem.propTypes = {
+    to: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    className: PropTypes.string,
 }
 
 export default NavItem;
