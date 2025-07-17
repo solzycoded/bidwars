4415791cbe855e976741b1930e9ce8c30843c433
solzycoded <solomonfidelis012@gmail.com>
2025-07-17 17:30:30 +0100

    included the last component, Alert and converted it to react code, also included it in the layout

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../components/Nav/Nav.js";
+import Alert from "../components/Alert/Alert.js";
 
 const Layout = function() {
     return (
         <>
             <Nav />
             <Outlet />
+            <Alert />
         </>
     )
 }
