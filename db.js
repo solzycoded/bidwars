df9b78e91b2c07dd4936166e2bc0a9d18303c840
solzycoded <solomonfidelis012@gmail.com>
2025-07-27 22:37:16 +0100

    installed mongoose and replaced mongodb connection with mongoose

--- db.js
+++ db.js
@@ -1,20 +1,18 @@
 // create and initialize database
-import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
 async function connectDB(startServer) {
     try {
-        const { MONGO_DB_PASSWORD, MONGO_USERNAME, MONGO_APP_NAME } = process.env;
+        const { MONGO_DB_PASSWORD, MONGO_USERNAME, MONGO_APP_NAME } = process.env; // mongod db connection variables
 
-        const url = `mongodb+srv://${MONGO_USERNAME}:${MONGO_DB_PASSWORD}@${MONGO_APP_NAME}.1w4ft.mongodb.net/?retryWrites=true&w=majority&appName=${MONGO_APP_NAME}`;
+        const url = `mongodb+srv://${MONGO_USERNAME}:${MONGO_DB_PASSWORD}@${MONGO_APP_NAME}.1w4ft.mongodb.net/?retryWrites=true&w=majority&appName=${MONGO_APP_NAME}`; // connection url
 
-        const client = await MongoClient.connect(url, { serverSelectionTimeoutMS: 5000 }); // connect to mongodb and start it
+        await mongoose.connect(url); // connect to mongodb
         console.log("Database Created");
 
         startServer(); // start the nodejs server
-
-        client.close(); // close the connection
     } catch (err) {
         console.error("MongoDB connection error:", err);
     }
