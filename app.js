bdfdca7e6642a31d8d45196cf9a98f348da75033
solzycoded <solomonfidelis012@gmail.com>
2025-07-27 21:36:02 +0100

    installed dotenv for storing and using environmental variables.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { MongoClient } from "mongodb";
 
 const app = express();
 
+const url = "";
+MongoClient.connect()
 const port = 3000;
-
 app.listen(port, () => {
     console.log("App started on port: ", port);
 });
