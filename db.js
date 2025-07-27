dabc4553e59da0053498a918816a41d578f7aa3c
solzycoded <solomonfidelis012@gmail.com>
2025-07-27 22:25:23 +0100

    moved the mongodb connection code to a new file (db.js)

--- db.js
+++ db.js
@@ -3,12 +3,12 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
-const { MONGO_DB_PASSWORD, MONGO_USERNAME, MONGO_APP_NAME } = process.env;
-
-const url = `mongodb+srv://${MONGO_USERNAME}:${MONGO_DB_PASSWORD}@${MONGO_APP_NAME}.1w4ft.mongodb.net/?retryWrites=true&w=majority&appName=${MONGO_APP_NAME}`;
-
 async function connectDB(startServer) {
     try {
+        const { MONGO_DB_PASSWORD, MONGO_USERNAME, MONGO_APP_NAME } = process.env;
+
+        const url = `mongodb+srv://${MONGO_USERNAME}:${MONGO_DB_PASSWORD}@${MONGO_APP_NAME}.1w4ft.mongodb.net/?retryWrites=true&w=majority&appName=${MONGO_APP_NAME}`;
+
         const client = await MongoClient.connect(url, { serverSelectionTimeoutMS: 5000 }); // connect to mongodb and start it
         console.log("Database Created");
 
