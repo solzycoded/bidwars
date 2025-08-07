1a08b4417978eb046df076d2e9e6bf0c62ca8ba5
solzycoded <solomonfidelis012@gmail.com>
2025-08-07 15:21:01 +0100

    started re-factoring the signup function in AuthController

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 /* RESP: ONLY DEFINES ROUTES AND MIDDLEWARS */
 import express from "express";
+import bodyParser from "body-parser";
 
 /* routes*/
 import authRoute from "./routes/auth.js";
 
-const app = express(); 
+const app = express();
+
+/* app level middleware */
+app.use(bodyParser.urlencoded({ extended: true })); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // allows body as json to be parsed to req parameter
 
 /* app routes (router level middlewares)*/
 app.use('/auth', authRoute); // authentication routes
+
 
 export default app;
--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -13,8 +13,6 @@ const signup = (req: Request, res: Response): void | Response => {
         return res.status(406).json({ success: false, data: { message: "Some Fields are missing" } });
     }
 
-    console.log({ email, username, password });
-    
     // const token = App.token();
     // const role  = "user";
     // const data  = [email, username, password, role, token];
