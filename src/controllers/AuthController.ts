f16dbe906fbfd65bcd8d81eaaa7be8ca99e14602
solzycoded <solomonfidelis012@gmail.com>
2025-08-07 21:56:34 +0100

    installed bcrypt for hashing and the logic for hashing the user's password.

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { validationResult, Result, ValidationError, matchedData } from "express-validator";
+import bcrypt from "bcrypt";
 
 import CustomError from "../utils/CustomError.js"; // Adjust the path as needed
 import { UserType } from "../utils/Types.ts";
@@ -15,7 +16,27 @@ const signup = (req: Request, res: Response): void | Response => {
 
     const { username, email, password }: UserType = matchedData(req);
 
+    bcrypt.hash(password, 10, (err: object, hash: string): void | Response => {
+        if (err) {
+            const error = new CustomError("Invalid password.", 500, []);
 
+            throw error;
+        }
+
+        
+        // user.create(data, (err, result) => {
+        //     try{
+        //         if(result.insertId==undefined){
+        //             res.status(201).json({ success: false, data: { message: "Username or Email already exists" } });
+        //         }
+        //         else{
+        //             res.status(201).json({ success: true, data: { token: token, username, role: role, id: result.insertId } });
+        //         }
+        //     } catch(err) {
+        //         return res.status(201).json({ success: false, data: { message: "Username or Email already exists", error: err} });
+        //     }
+        // });
+    });
 };
 
 export default {
