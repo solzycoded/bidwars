ecb4ea1d91c8dd56a162c14484e385ea4d38561b
solzycoded <solomonfidelis012@gmail.com>
2025-09-04 19:13:16 +0100

    re-installed npm project

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,7 +8,7 @@ const Login = () => {
     const submitLogin = async (e) => {
         e.preventDefault();
 
-        const res = await fetch("http://localhost:3000/auth/login", {
+        const res = await fetch("http://localhost:4500/auth/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ usernameOrEmail, password }),
