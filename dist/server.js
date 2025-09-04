dde510b67987f846d3791a03f635bc36f60a0082
solzycoded <solomonfidelis012@gmail.com>
2025-09-04 18:49:36 +0100

    changed port number to use one from the .env file

--- dist/server.js
+++ dist/server.js
@@ -4,7 +4,7 @@ import connectDB from "./db.js";
 /* start server and connect db */
 if (process.env.NODE_ENV !== 'test') {
     connectDB().then(() => {
-        const port = 3000;
+        const port = (process.env.PORT || 4500);
         app.listen(port, () => {
             console.log('Server started on port: ', port);
         });
