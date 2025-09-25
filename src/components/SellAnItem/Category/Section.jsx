f6277d526e78fd240b950896372f5482de5e87d0
solzycoded <solomonfidelis012@gmail.com>
2025-09-25 10:55:35 +0100

    refactored vanilla js code for category front-end functionality, removed the category.js file and it's reference in index.html

--- src/components/SellAnItem/Category/Section.jsx
+++ src/components/SellAnItem/Category/Section.jsx
@@ -3,22 +3,18 @@ import CategoryItem from "./Item.jsx";
 
 const Category = () => {
     const [ categories, setCategories ] = useState(null);
-
-    const onCategorySelected = (category) => {
-        // handle category selection here
-        console.log("Selected category:", category);
-    };
+    const [ selectedCategoryIndex, setSelectedCategoryIndex ] = useState(null);
 
     useEffect(() => {
         const displayCategories = (res) => {
-            const listOfCategories = res.categories;
+            const listOfCategories = res;
             setCategories(listOfCategories);
         }
 
         const _categories = [
-            {_id: "s882ialjd", name: "cars"},
-            {_id: "s882ialjd", name: "shoes"},
-            {_id: "s882ialjd", name: "art"},
+            {_id: "s882ialjd1123", name: "cars"},
+            {_id: "s882ialjd5434", name: "shoes"},
+            {_id: "s882ialjd9087", name: "art"},
         ];
 
         displayCategories(_categories);
@@ -31,19 +27,18 @@ const Category = () => {
                 <div className="text-start mb-4">
                     <h5>Category</h5>
                         {
-                            categories?.map((category) => {
-                                return (
-                                    <button
-                                        type="button"
-                                        className="col-12 col-sm-12 col-md-4 mb-3 sell-an-item-category-item"
-                                        key={category._id}
-                                        onClick={() => onCategorySelected(category)}
-                                    >
-                                        <CategoryItem categoryName={category.name} />
-                                        <input type="hidden" name="category_id" className="category_id" value={category._id} />
-                                    </button>
-                                );
-                            })
+                            categories?.map((category, i) => (
+                                <button
+                                    type="button"
+                                    className={`col-12 col-sm-12 col-md-4 mb-3 sell-an-item-category-item ${selectedCategoryIndex===i ? 'selected-category' : ""}`}
+                                    key={category._id}
+                                    onClick={() => setSelectedCategoryIndex(i)}
+                                    aria-pressed={selectedCategoryIndex === i}
+                                >
+                                    <CategoryItem categoryName={category.name} />
+                                    <input type="hidden" name="category_id" className="category_id" value={category._id} />
+                                </button>
+                            ))
                         }
 
                 </div>
